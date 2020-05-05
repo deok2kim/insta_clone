9311667a280d06170319c0fcbad63e8d6f139cb8
deokgikim <dukgi77@naver.com>
2020-05-05 16:11:04 +0900

    Photo | create generic view & url

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+# include() : 그냥들어오는게 있으면 뒤에 가로로 보내라
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("photo.urls")),
 ]
